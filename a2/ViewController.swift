ee2c2ed8406978c93f6ca2d05ec4bec28aee4ac6
btm0028 <btm0028@auburn.edu>
2023-01-29 18:37:36 -0500

    Initial Commit

---
+++ a2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  a2
+//
+//  Created by Brandon  Miller on 1/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
